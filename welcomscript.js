3cbaff45e700826c06afa2ad0b58c91111a06a69
Harkirat47 <har.ki.kirat@gmail.com>
2023-03-27 20:01:04 -0700

    all shubhays fault

--- welcomscript.js
+++ welcomscript.js
@@ -7,10 +7,6 @@ window.onmousedown = e => {
 window.onmousemove = e =>{
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
         maxDelta = window.innerWidth / 2;
-    
     const percentage = (mouseDelta/maxDelta)*100;
-
     track.style.transform =  `translate(${percentage}%, -50%)`;
-
-
-    
+}
